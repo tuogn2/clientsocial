41978ddb8b33b1cde096cb0ed6ddb7332a228d64
tuogn2 <95692889+tuogn2@users.noreply.github.com>
2023-05-04 12:46:22 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter, Link, Route, Routes, redirect } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/pages/Homepage';
 import Defaultlayout from './components/layouts/Defaultlayout';
 import Authe from './components/pages/Auth';
 import Notfound from './components/pages/Notfound';
 import Userlayout from './components/layouts/Userlayout';
 import Profileuser from './components/pages/Profileuser';
-import { useEffect, useState } from 'react';
 function App() {
   
   return (
